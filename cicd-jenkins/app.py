fd85de69bb76cb4d5da58d7d4ca6ec91575d360d
prashant1926misra <prashant2611misra@gmail.com>
2025-06-21 07:36:09 +0000

    update app.py

--- cicd-jenkins/app.py
+++ cicd-jenkins/app.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
+
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)  #Exposes metrics 
 
 @app.route('/')
 def hello():
