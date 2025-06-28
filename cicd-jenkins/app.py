b0b504a883f3001e9cc9bf3cd5e2d497ed2b7224
prashant1926misra <prashant2611misra@gmail.com>
2025-06-28 08:23:12 +0000

    add /break and /heal in app.py to simulate probe failures

--- cicd-jenkins/app.py
+++ cicd-jenkins/app.py
@@ -2,11 +2,27 @@ from flask import Flask
 from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
-metrics = PrometheusMetrics(app)  #Exposes metrics 
+metrics = PrometheusMetrics(app)
+
+
+app_is_broken = {"status": False}
 
 @app.route('/')
-def hello():
-    return "Hello from Dockerized Flask!"
+def root():
+    if app_is_broken["status"]:
+        return "Simulated failure", 500
+    return "Hello from Dockerized Flask!", 200
+
+
+@app.route('/break')
+def break_app():
+    app_is_broken["status"] = True
+    return "App marked as broken", 200
+
+@app.route('/heal')
+def heal_app():
+    app_is_broken["status"] = False
+    return "App marked as healthy", 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
