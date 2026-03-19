928d126d6ca0bd53c7156e4fda22ced0b6b0acd0
prashant1926misra <prashantnau2611@gmail.com>
2026-03-20 00:03:14 +0530

    Add variables and required providers for GKE cluster module

---
+++ infra-bootstrap/modules/gke-cluster/variables.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+  description = "The GCP Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP region"
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "The name of the GKE cluster"
+  type        = string
+}
