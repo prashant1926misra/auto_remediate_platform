8c66ff7354ce89dd228fe1793288efb7f05990b6
prashant1926misra <prashantnau2611@gmail.com>
2026-08-16 19:13:17 +0530

    Revert machine type in node configuration to e2-medium in GKE cluster

--- infra-bootstrap/modules/gke-cluster/main.tf
+++ infra-bootstrap/modules/gke-cluster/main.tf
@@ -33,7 +33,7 @@ resource "google_container_node_pool" "primary_nodes" {
   node_count = 1
 
   node_config {
-    machine_type = "e2-standard-4"
+    machine_type = "e2-medium"
 
     disk_type    = "pd-balanced"
     disk_size_gb = 50
